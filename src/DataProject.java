549c849be2d873a0816522c7ae912e47beb21d29
Trey <trey@morris2morris.com>
2021-04-29 16:51:52 -0400

    Initial commit

---
+++ src/DataProject.java
@@ -0,0 +1,33 @@
+import core.data.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DataProject
+{
+    public static void main(String[] args) 
+    {
+        
+        DataSource electionResults = DataSource.connect("lib/us-election-2012-results-by-county - us-election-2012-results-by-county.csv");
+        electionResults.load();
+
+        ArrayList<Results> results = electionResults.fetchList("Results","FirstName","LastName","Party","CountyName","StateCode","Votes");
+       
+        
+        
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a County name (CASE SENSITIVE): ");
+        String county = sc.next();
+        System.out.println("Election Results in " + county);
+        for (Results el : results) 
+        {
+            if (el.isLocatedInCounty(county)) {
+               System.out.println(el.getName() +" Representing the " + el.partyAbrev() + " party received " + el.getVoteCount() + " votes in " + el.countyName() +" County, in the state of " + el.stateName() + " in the 2012 election.");
+            }
+            
+        }
+    }
+}
+
+
+
---
+++ src/Results.java
@@ -0,0 +1,49 @@
+class Results 
+{
+    private String firstName;
+    private String lastName;
+    private String partyAbrev;
+    private String countyName;
+    private String stateCode;
+    private int voteCount;
+    private String space = " ";
+
+    Results(String firstName, String lastName, String partyAbrev, String countyName, String stateCode, int voteCount)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.partyAbrev = partyAbrev;
+        this.countyName = countyName;
+        this.stateCode = stateCode;
+        this.voteCount = voteCount;
+    }
+
+    
+   public String partyAbrev()
+    { 
+    return partyAbrev;
+    }
+    public String countyName()
+    { 
+    return countyName;
+    }
+    public String stateName()
+    { 
+    return stateCode;
+    }
+    public String getName() 
+    { 
+    return firstName + space + lastName;
+    }
+ 
+ 
+    public boolean isLocatedInCounty(String st) 
+    {
+    return this.countyName.equals(st);
+    }
+
+    public int getVoteCount()
+    {
+        return voteCount;
+    }
+}
